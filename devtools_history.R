6fe167503b8ba6f06f04ec1ea3d3139039abbbbc
Yves Gallien <yves.gallien@gmail.com>
2019-11-28 12:17:25 +0100

    Modification des gitignore

--- devtools_history.R
+++ devtools_history.R
@@ -2,3 +2,6 @@ usethis::use_package("stats")
 usethis::use_package("survminer")
 
 usethis::use_build_ignore("devtools_history.R")
+usethis::use_git_ignore("devtools_history.R")
+
+usethis::use_git_ignore("YPJ.Rproj")
