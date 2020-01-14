00541c9d80774b051dd37eb7886a3fd1cb467be1
Yves Gallien <yves.gallien@gmail.com>
2020-01-14 10:20:02 +0100

    Fixed names

--- R/survival_univariate.r
+++ R/survival_univariate.r
@@ -4,6 +4,8 @@
 #'   time to event and one variable for event indicator.
 #' @param time Name of the variable used for time to event.
 #' @param event Name of the column used for event indicator.
+#' @param names Names of the variables to display. Lenght must be minus 2 the
+#'   number of column of data, excluding time and event
 #' @param test Which test to use for p-value, possible values are "LRT" for
 #'   Likelihood Ratio Test, "Wald" for Wald Test and "LogRank" for Log-Rank
 #'   Test"
